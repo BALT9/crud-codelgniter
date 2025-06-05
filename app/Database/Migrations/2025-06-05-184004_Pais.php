484e40c0de058c101a4bdb545c5fbb1dbcf24cf2
willy <willybalt.2017@gmail.com>
2025-06-05 18:42:30 -0400

    se agregaron las migraciones

--- app/Database/Migrations/2025-06-05-184004_Pais.php
+++ app/Database/Migrations/2025-06-05-184004_Pais.php
@@ -8,11 +8,34 @@ class Pais extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+
+            'codigo_pais' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+
+            'nombre_pais' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'codigo_continente' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+
+        ]);
+
+        $this->forge->addKey('codigo_pais',true);
+        $this->forge->addForeignKey('codigo_continente','continente','codigo_continente', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('pais');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('pais');
     }
 }
--- app/Database/Migrations/2025-06-05-184012_Estado.php
+++ app/Database/Migrations/2025-06-05-184012_Estado.php
@@ -8,11 +8,37 @@ class Estado extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'codigo_estado' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nombre_estado' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'codigo_continente' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+            'codigo_pais' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('codigo_estado', true);
+        $this->forge->addForeignKey('codigo_continente', 'continente', 'codigo_continente', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('codigo_pais', 'pais', 'codigo_pais', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('estado');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('estado');
     }
 }
